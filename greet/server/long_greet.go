411d940e46f3b10f60598c85c235cb26a48e1250
Nguyen Dang Quy <dangquyitt@gmail.com>
2024-05-25 15:59:07 +0700

    LongGreet function example for client streaming

---
+++ greet/server/long_greet.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"strings"
+
+	pb "github.com/dangquyitt/go-learn-grpc/greet/proto"
+)
+
+func (s *Server) LongGreet(stream pb.GreetService_LongGreetServer) error {
+	log.Println("LongGreet function was invoked")
+
+	var resBuilder strings.Builder
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.GreetResponse{
+				Result: resBuilder.String(),
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v\n", err)
+		}
+
+		resBuilder.WriteString(fmt.Sprintf("Hello %s\n", req.FirstName))
+	}
+
+	return nil
+}
