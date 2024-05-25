50a5f6210eda92a1c64f1b2bb322a2e64ded91f1
Nguyen Dang Quy <dangquyitt@gmail.com>
2024-05-25 14:50:15 +0700

    Create rpc GreetManyTimes with server streaming

---
+++ greet/client/greet_many_times.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/dangquyitt/go-learn-grpc/greet/proto"
+)
+
+func doGreetManyTimes(c pb.GreetServiceClient) {
+	log.Println("doGreetManyTimes was invoked")
+
+	req := &pb.GreetRequest{
+		FirstName: "Johnny",
+	}
+
+	stream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes: %v\n", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading the stream: %v\n", err)
+		}
+
+		log.Printf("GreetManyTimes: %v\n", msg.Result)
+	}
+}
