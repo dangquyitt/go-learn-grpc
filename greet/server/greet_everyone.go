960ffe42fc417d19b54c5325027d3b113b8baeec
Nguyen Dang Quy <dangquyitt@gmail.com>
2024-05-25 16:14:54 +0700

    Example GreetEveryone using Bidirectional streaming

---
+++ greet/server/greet_everyone.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	pb "github.com/dangquyitt/go-learn-grpc/greet/proto"
+)
+
+func (s *Server) GreetEveryone(stream pb.GreetService_GreetEveryoneServer) error {
+	log.Println("GreetEveryone function was invoked")
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v\n", err)
+		}
+
+		res := fmt.Sprintf("Hello %s", req.FirstName)
+		err = stream.Send(&pb.GreetResponse{
+			Result: res,
+		})
+
+		if err != nil {
+			log.Fatalf("Error while sending data to client: %v\n", err)
+		}
+	}
+
+	return nil
+}
