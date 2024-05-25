624a175b3d828369e85416e6f7560c772f810b9c
Nguyen Dang Quy <dangquyitt@gmail.com>
2024-05-25 13:29:05 +0700

    Create basic client-server for greet

---
+++ greet/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/dangquyitt/go-learn-grpc/greet/proto"
+	"google.golang.org/grpc"
+)
+
+const addr = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+	log.Printf("Listening on %s\n", addr)
+	s := grpc.NewServer()
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
