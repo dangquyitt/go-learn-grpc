6f0645204584146b950212f7167cda4242c03377
Nguyen Dang Quy <dangquyitt@gmail.com>
2024-05-25 14:12:01 +0700

    Calculator service Unary API

---
+++ calculator/client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/dangquyitt/go-learn-grpc/calculator/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const addr = "0.0.0.0:50052"
+
+func main() {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v\n", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewCalculatorServiceClient(conn)
+
+	doCalculate(c)
+}
